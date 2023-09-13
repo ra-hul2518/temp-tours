3308362fe1fde164e3346403293aebc58616ba1d
Rahul Pandey <rahul25012003@gmail.com>
2023-09-13 14:57:00 +0530

    first tour

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './App.css'
+import Nav from './components/navbar'
+import Hero from './components/hero'
+import About from './components/about'
+import Services from './components/services'
+import Tours from './components/tours'
+import Footer from './components/footer'
+const App = () => {
+  return (
+    <>
+    <Nav/>
+    <Hero/>
+    <About/>
+    <Services/>
+    <Tours/>
+    <Footer/>
+
+
+   
+       
+
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,104 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+import tour4 from "./images/tour-4.jpeg";
+import tour5 from "./images/tour-5.jpeg";
+import tour6 from "./images/tour-6.jpeg";
+
+
+export const pageLinks = [
+  { id: 1, href: "#home", text: "home" },
+  { id: 2, href: "#about", text: "about" },
+  { id: 3, href: "#services", text: "services" },
+  { id: 4, href: "#tours", text: "tours" },
+];
+
+export const socialLinks = [
+  { id: 1, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+  { id: 2, href: "https://www.facebook.com", icon: "fab fa-facebook" },
+  { id: 3, href: "https://www.squarespace.com", icon: "fab fa-squarespace" },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "lorem ipsum hue hue",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "lorem ipsum hue hue",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: "lorem ipsum hue hue",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: "august 26th, 2023",
+    title: "Tibet Adventure",
+    info: "lorem ipsum hue hue hue hue hue lorem",
+    location: "china",
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: "august 26th, 2023",
+    title: "Tibet Adventure",
+    info: "lorem ipsum hue hue hue hue hue lorem",
+    location: "France",
+    duration: 3,
+    cost: 2900,
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: "august 26th, 2023",
+    title: "Tibet Adventure",
+    info: "lorem ipsum hue hue hue hue hue lorem",
+    location: "Brazil",
+    duration: 4,
+    cost: 1900,
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: "august 26th, 2023",
+    title: "Tibet Adventure",
+    info: "lorem ipsum hue hue hue hue hue lorem",
+    location: "Kenya",
+    duration: 2,
+    cost: 1100,
+  },
+  {
+    id: 5,
+    image: tour5,
+    date: "august 26th, 2023",
+    title: "Tibet Adventure",
+    info: "lorem ipsum hue hue hue hue hue lorem",
+    location: "Kenya",
+    duration: 2,
+    cost: 1100,
+  },
+  {
+    id: 6,
+    image: tour6,
+    date: "august 26th, 2023",
+    title: "Tibet Adventure",
+    info: "lorem ipsum hue hue hue hue hue lorem",
+    location: "Kenya",
+    duration: 2,
+    cost: 1100,
+  },
+];
